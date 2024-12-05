7bedcd658285ff3a7268571f3dd79faa0d280964
Diksha Chaudhary <97454110+dikshachaudhary25@users.noreply.github.com>
2024-12-05 19:37:21 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum{
+    public static void main(String [] args){
+        System.out.print("Input a = ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("Input b = ");
+        int b = sc.nextInt();
+        int c;
+        c=a+b;
+        System.out.print("Sum = ");
+        System.out.print(c);
+    }
+}
