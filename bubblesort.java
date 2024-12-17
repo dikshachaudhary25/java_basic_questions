30fe7447018b287f9a47b4e929ffa05060cf79fb
Diksha Chaudhary <97454110+dikshachaudhary25@users.noreply.github.com>
2024-12-18 02:05:55 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class bubblesort{
+    public static void sort1(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length-1; i++){
+            for(int j= 0; j<arr.length-i-1; j++){
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Performing bubble sort");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the  array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter the elements : ");
+        for(int i =0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        sort1(arr);
+        
+    }
+    
+}
